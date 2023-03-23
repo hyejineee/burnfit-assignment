5c7ed311704837667fb6565a558a9656c62ceb3e
hyejineee <dev.hyejin04@gmail.com>
2023-03-23 20:21:00 +0900

    feat : level2 작업

--- src/screens/Calendar.tsx
+++ src/screens/Calendar.tsx
@@ -1,11 +1,9 @@
-import {Text} from 'react-native';
 import styled from 'styled-components/native';
 import Calerdar from '../components/Calendar';
 
 export default function CalendarScreen() {
   return (
     <Wrapper>
-      <Text>Calendar</Text>
       <Calerdar />
     </Wrapper>
   );
